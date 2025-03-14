c02ad6c31001dcca07983b0505d6cff3b47f40d4
alupoaiemircea <mircea@axiologic.net>
2025-03-14 09:36:34 +0200

    fix smokeTest.js

--- tests/smokeTest.js
+++ tests/smokeTest.js
@@ -1,6 +1,6 @@
 require("./testInit/clean.js");
 
-async function loginFlow() {
+async function smokeTest() {
     await $$.clean();
     await $$.registerPlugin("StandardPersistence", "../../plugins/StandardPersistence.js");
     await $$.registerPlugin("UserLogin", "../../plugins/UserLogin.js");
@@ -26,13 +26,27 @@ async function loginFlow() {
         console.log("User logged out");
         let result = await UserLogin.checkSessionId(email, "");
         console.assert(!result, "SessionId check failed");
+
+        let userInfo = await UserLogin.getUserInfo(email);
+        console.assert(userInfo === undefined, "userInfo check failed");
+        userInfo = {};
+        userInfo.firstName = "John";
+        userInfo.lastName = "Doe";
+        userInfo.spaces = ["space1", "space2"];
+        userInfo.roles = {
+            "space1": "owner",
+            "space2": "read"
+        }
+        await UserLogin.setUserInfo(email, userInfo);
+        let userInfoCheck = await UserLogin.getUserInfo(email);
+        console.assert(userInfoCheck === userInfo, "userInfo check failed");
         await UserLogin.shutDown();
     } else {
        throw new Error("User already exists");
     }
 }
 
-loginFlow().then(() => {
+smokeTest().then(() => {
 }).catch(console.error);
 
 
