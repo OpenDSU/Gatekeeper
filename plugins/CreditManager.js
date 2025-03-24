74b07a524d389b6e9f2c23203744d7e66d26fceb
adiganga2002 <38322305+adiganga2002@users.noreply.github.com>
2025-03-24 11:58:25 +0200

    refactor and cleanup

--- plugins/CreditManager.js
+++ plugins/CreditManager.js
@@ -1,51 +1,8 @@
-const coreUtil = require('../Persisto/src/persistence/utils');
-let MathMoney = coreUtil.MathMoney;
-let parseThresholds = coreUtil.parseThresholds;
-let computeStakeSublinear = coreUtil.computeStakeSublinear;
-let computePercent = coreUtil.computePercent;
-
 async function CreditManager() {
     let self = {};
     let persistence = await $$.loadPlugin("StandardPersistence");
     let AppSpecificPlugin = await $$.loadPlugin("AppSpecificPlugin");
     let tickInterval = undefined;
-
-    self.validateUser = async function (id, level) {
-        let user = await persistence.getUser(id);
-        if (user.level === 0 && level > 0) {
-            let unlockingPercent = 1;
-            switch (level) {
-                case 1:
-                    unlockingPercent = 0.1;
-                    break;
-                default:
-                    unlockingPercent = 1;
-            }
-            let lockedAmountUntilValidation = user.lockedAmountUntilValidation * unlockingPercent;
-            user.lockedAmountUntilValidation -= lockedAmountUntilValidation;
-            if (lockedAmountUntilValidation) {
-                persistence.unlockPoints(id, lockedAmountUntilValidation, "User validation rewarding the user " + user.id);
-            }
-            let rewardInvitingUserId = user.invitingUserID;
-            if (rewardInvitingUserId) {
-                let lockedAmountForInvitingUser = user.lockedAmountForInvitingUser * unlockingPercent;
-                user.lockedAmountForInvitingUser -= lockedAmountForInvitingUser;
-                if (lockedAmountForInvitingUser) {
-                    persistence.unlockPoints(rewardInvitingUserId, lockedAmountForInvitingUser, "User validation rewarding the inviter " + user.id);
-                }
-            }
-
-            await persistence.setUserLevel(id, level);
-            if (user.lockedAmountUntilValidation > 0) {
-                await self.confiscateLockedPoints(id, user.lockedAmountUntilValidation, "After setting the user level");
-            }
-            if (user.lockedAmountForInvitingUser > 0) {
-                await self.confiscateLockedPoints(user.invitingUserID, user.lockedAmountForInvitingUser, "After setting the user level");
-            }
-            await persistence.updateUser(id, {lockedAmountUntilValidation: 0, lockedAmountForInvitingUser: 0});
-        }
-        user.level = level;
-    }
 
     self.safeTransfer = function (amount, from, to) {
         if (amount === 0) {
@@ -92,8 +49,10 @@ async function CreditManager() {
         let user = await persistence.createUser({
             email,
             name,
+            invitingUserID: referrerId,
             level: 0,
             lockedAmountUntilValidation: 0,
+            lockedAmountForInvitingUser: 0
         });
         await AppSpecificPlugin.rewardUser(user, referrerId);
         return user;
